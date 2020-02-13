3f8c985f87fdbe63e2e6c6b942fe25e00528ba9f
Zoe Gonzales <zgonzales@joinhandshake.com>
2020-02-13 11:19:05 -0700

    hello world basics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: '¡Hola, mundo!'
+    end
+
+    def goodbye
+        render html: 'goodbye for now'
+    end
 end
